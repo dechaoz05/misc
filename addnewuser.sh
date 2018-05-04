56c7388f2134bf0ef99c897219dfb71143ee4b54
dechaoz05 <dechaoz005@gmail.com>
2018-05-04 13:28:43 -0700

    newsuperuser.sh
    create new superuser in ubuntu

---
+++ addnewuser.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+
+username="deanchao"
+
+if id -u ${username}
+then
+        echo "user ${usrename} already exist, do not add"
+else
+        adduser --disabled-password --gecos "" ${username}
+fi
+
+group="sudo"
+if getent group ${group} | grep &>/dev/null "\b${username}\b"
+then
+    echo "user ${username} exists in ${group} group, do not add"
+else
+        usermod -aG ${group} ${username}
+fi
+
+sudoersfile="/etc/sudoers.d/90-cloud-init-users"
+if grep --quiet ${username}  ${sudoersfile}
+then
+        echo "user ${username} exists in sudoers list"
+else
+        echo "${username} ALL=(ALL) NOPASSWD:ALL" >> ${sudoersfile}
+
+fi
